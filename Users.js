542c293d3b279dc8eddfeaa3742667adf2ec35e2
ShruthiSaravanan <165787855+ShruthiSaravanan@users.noreply.github.com>
2024-04-04 20:03:26 +0530

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+let gender = 'male';
+let searchCity = 'Louisville'; 
+
+fetch('https://dummyjson.com/users')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); 
+  })
+  .then(data => {
+       
+    let filteredData;
+    if(data.users){
+     const userList = data.users;
+     filteredData = userList.filter((users) => {
+            return users.gender === gender && users.address.city === searchCity;
+          });
+    }
+      
+    filteredData.sort((a, b) => a.firstName.localeCompare(b.firstName)); // Sort the array directly
+    console.log(filteredData);
+    
+  });
+
+
